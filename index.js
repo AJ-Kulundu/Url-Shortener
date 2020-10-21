6029d1ddfb9834f4718a6fc9b33d98ee08dec8f5
AJ-Kulundu <jameskulundu@gmail.com>
2020-10-21 20:40:07 +0300

    Connecting the application to the mongodb database and editing the ejs file

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 
 PORT = process.env.PORT
 
-app.set('viewengine','ejs');
+app.set('view engine','ejs');
+mongoose.connect(process.env.DATABASE_URI,{useNewUrlParser:true,useUnifiedTopology:true},()=> console.log('Connected to database'))
 
 app.get('/',(req,res) =>{
-    res.send('Hello world')
+    res.render('index',{variable:'hello world'})
 })
 
 app.listen(PORT, ()=> console.log(`Server started on localhost:${PORT}`))
