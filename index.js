500a5df51e7e4049c1268eac3f12abdfc7c0c5b7
AJ-Kulundu <jameskulundu@gmail.com>
2020-10-22 22:18:11 +0300

    Linking the frontend to the backend to display the links

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ const mongoose = require('mongoose');
 const ShortUrl = require('./models/url')
 
 const app = express();
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 PORT = process.env.PORT
 
 app.set('view engine','ejs');
 mongoose.connect(process.env.DATABASE_URI,{useNewUrlParser:true,useUnifiedTopology:true},()=> console.log('Connected to database'))
 
-app.get('/',(req,res) =>{
-    res.render('index',{variable:'hello world'})
+app.get('/', async (req,res) =>{
+    const allData = await ShortUrl.find()
+	res.render('index', { shortUrls: allData })
 });
 
 app.post('/short', async (req,res) =>{
-    //insert record into database
+    const fullUrl = req.body.fullUrl
+    console.log('URL Requested: ', fullUrl)
     const record = new ShortUrl({
-        full:'test'
+        full: fullUrl
     });
     await record.save();
-    res.json({short:short, ok : 1})
+    
+    res.redirect('/')
 })
 
 app.listen(PORT, ()=> console.log(`Server started on localhost:${PORT}`))
